0ce4c7351b2c002dabe8c470a1bd9322f7de0a49
Barkamol0422 <ganijonovbarkamol04222011@gmail.com>
2025-09-27 17:51:08 +0500

    Add files via upload

---
+++ act4.py
@@ -0,0 +1,12 @@
+def myfunction(n):
+    for i in range(0,n+1):
+        print("First loop")
+    j=1
+    while(j<=n+1):
+        print("Second loop")
+        j=j*2
+    for i in range(0,100):
+        print("Third loop")
+n=int(input("Enter a number: "))
+myfunction(n)
+print("It is quadratic")
